04225ac594f61baab26f381e3e1819ac52236435
Manoel Jr <120431500+Manoelpj@users.noreply.github.com>
2025-09-17 19:08:28 +0000

    feat: adicionar interface de Pilha e mensagem de excecao

---
+++ pilha/pilhaImplementacao/Pilha.java
@@ -0,0 +1,15 @@
+public interface Pilha{
+
+    public void push(Object objeto);
+
+    public Object pop() throws PilhaVaziaExcecao;
+
+    public Object top() throws PilhaVaziaExcecao;
+    
+    public int size();
+
+    public boolean isEmpty();
+}
+
+
+
