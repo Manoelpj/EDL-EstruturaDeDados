3c8b80ff4afe5ad3ba4d03b34a07555deb052da8
Manoel Jr <120431500+Manoelpj@users.noreply.github.com>
2025-09-17 19:08:57 +0000

    feat: adicionar implementacao de Pilha

---
+++ pilha/pilhaImplementacao/PilhaArray.java
@@ -0,0 +1,55 @@
+public class PilhaArray implements Pilha{
+    private int capacidade;
+    private int fatorCrescimento;
+    private int topo;
+    private Object[] array;
+
+    public PilhaArray(int capacidade, int fatorCrescimento){
+        this.capacidade = capacidade;
+        this.fatorCrescimento = fatorCrescimento;
+        this.topo = -1;
+        this.array = new Object[capacidade];
+    }
+
+    public void push(Object objeto){
+        if (topo+1 == capacidade) {            
+            if(fatorCrescimento == 0){
+                capacidade*=2;
+            }else if(fatorCrescimento > 0){ //fatorCrescimento não pode ser menor que zero, né? Pq um array iria diminuir sua capacidade, meio sem lógica
+                capacidade+=fatorCrescimento;
+            }
+            Object[] array2 = new Object[capacidade];
+            for(int i = 0; i < array.length; i++){
+                array2[i] = array[i];
+            }
+            array = array2;
+        }
+        array[++topo] = objeto;
+    }
+
+    public Object pop() throws PilhaVaziaExcecao{ //tem que retornar o topo atual ou o topo antigo? Se for o topo antigo está correto, porém se for o topo atual(dps do pop apagar o atual) está incorreto.
+        if(topo == -1){
+            throw new PilhaVaziaExcecao("A pilha está vazia");
+        }
+        return array[topo--];
+    }
+
+    public Object top() throws PilhaVaziaExcecao{
+        if(topo == -1){
+            throw new PilhaVaziaExcecao("A pilha está vazia");
+        }
+        return array[topo];
+    }
+
+    public int size(){
+        return topo+1;
+    }
+
+    public boolean isEmpty(){
+        if(topo==-1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
