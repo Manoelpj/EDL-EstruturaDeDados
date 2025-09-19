09a68c8841a6cd379028d87dc135f2d475e8c99e
Manoelpj <manoelpinto2005@gmail.com>
2025-09-19 00:44:22 -0300

    feat: adicionar main para testes de métodos da pilha rubro negra

---
+++ pilha/pilhaImplementacao/PilhaMainRubroNegra.java
@@ -0,0 +1,23 @@
+public class PilhaMainRubroNegra {
+    public static void main(String[] args) {
+        PilhaRubroNegra pilhaRN = new PilhaRubroNegra(5);
+
+        pilhaRN.pushRubro(1);
+        pilhaRN.pushRubro(2);
+        pilhaRN.pushRubro(3);
+        pilhaRN.pushRubro(4);
+        pilhaRN.pushRubro(5);
+        pilhaRN.pushRubro(6);
+        pilhaRN.pushRubro(7);
+        pilhaRN.pushRubro(8);
+        pilhaRN.pushRubro(9);
+        pilhaRN.pushRubro(10);
+
+
+        // pilhaRN.popRubro();
+        // pilhaRN.popRubro();
+        // System.out.println(pilhaRN.popRubro());
+
+        pilhaRN.sout();
+    }
+}
