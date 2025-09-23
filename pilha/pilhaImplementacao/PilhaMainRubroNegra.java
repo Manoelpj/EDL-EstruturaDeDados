e54bd8d6c09a29bfde93163f33bc9557bdc88b55
Manoel Jr <120431500+Manoelpj@users.noreply.github.com>
2025-09-23 16:39:20 +0000

    chore: refazer testes da pilha rubro negra

--- pilha/pilhaImplementacao/PilhaMainRubroNegra.java
+++ pilha/pilhaImplementacao/PilhaMainRubroNegra.java
@@ -1,23 +1,54 @@
 public class PilhaMainRubroNegra {
     public static void main(String[] args) {
-        PilhaRubroNegra pilhaRN = new PilhaRubroNegra(5);
 
+        PilhaRubroNegra pilhaRN = new PilhaRubroNegra(10);
+
+        // PUSH AMBOS \\
         pilhaRN.pushRubro(1);
         pilhaRN.pushRubro(2);
         pilhaRN.pushRubro(3);
         pilhaRN.pushRubro(4);
         pilhaRN.pushRubro(5);
         pilhaRN.pushRubro(6);
-        pilhaRN.pushRubro(7);
-        pilhaRN.pushRubro(8);
-        pilhaRN.pushRubro(9);
-        pilhaRN.pushRubro(10);
+        pilhaRN.pushNegro("A");
+        pilhaRN.pushNegro("B");
+        pilhaRN.pushNegro("C");
+        pilhaRN.pushNegro("D");
+        pilhaRN.pushNegro("E");
+        pilhaRN.pushNegro("F");
 
 
-        // pilhaRN.popRubro();
-        // pilhaRN.popRubro();
-        // System.out.println(pilhaRN.popRubro());
+        // POP NEGRO \\ 
+        System.out.println(pilhaRN.popNegro()); 
+        System.out.println(pilhaRN.popNegro()); 
+        System.out.println(pilhaRN.popNegro());                 
+        pilhaRN.pushNegro("A");
+        pilhaRN.pushNegro("B");
+        pilhaRN.pushNegro("C");
+
+        // POP RUBRO \\
+        System.out.println(pilhaRN.popRubro()); 
+        System.out.println(pilhaRN.popRubro()); 
+        System.out.println(pilhaRN.popRubro());   
+        pilhaRN.pushRubro(1);
+        pilhaRN.pushRubro(2);
+        pilhaRN.pushRubro(3);   
+        
+        // SIZE AMBOS \\
+        System.out.println(pilhaRN.sizeNegro());
+        System.out.println(pilhaRN.sizeRubro());
+
+        // TOP AMBOS \\
+        System.out.println(pilhaRN.topRubro());
+        System.out.println(pilhaRN.topNegro());
+
+        // ISEMPTY AMBOS \\
+        System.out.println(pilhaRN.isEmptyRubro());
+        System.out.println(pilhaRN.isEmptyNegro());
+
+
 
         pilhaRN.sout();
+
     }
 }
