e51c6c6f89c688f08730e1b67d56aafa1d8306f2
Manoel Jr <120431500+Manoelpj@users.noreply.github.com>
2025-09-24 17:16:05 +0000

    feat: criar interface de TAD Fila

---
+++ fila/filaImplementacao/Fila.java
@@ -0,0 +1,12 @@
+public interface Fila{
+
+    public void enqueue(Object objeto);
+
+    public Object dequeue();
+
+    public Object first();
+
+    public int size();
+
+    public boolean isEmpty();
+}
