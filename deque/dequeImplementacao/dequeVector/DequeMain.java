7f9d4e9214acefde64c8c90563767bdc2411c1a1
Manoel Jr <120431500+Manoelpj@users.noreply.github.com>
2025-09-25 12:39:32 +0000

    feat: adicionar main para teste de dequeVector

---
+++ deque/dequeImplementacao/dequeVector/DequeMain.java
@@ -0,0 +1,28 @@
+package dequeVector;
+
+public class DequeMain{
+    public static void main(String[] args){
+        DequeVector deque = new DequeVector();
+
+        // System.out.println(deque.size()); 
+        // System.out.println(deque.isEmpty()); 
+
+
+        deque.addFirst(4);
+        deque.addFirst(3);
+        deque.addFirst(2);
+        deque.addFirst(1);
+
+        deque.addLast("A");
+        deque.addLast("B");
+        deque.addLast("C");
+
+        // System.out.println(deque.first()); 
+        // System.out.println(deque.last());  
+
+        System.out.println(deque.removeFirst()); 
+        System.out.println(deque.removeLast()); 
+
+        deque.sout();
+    }
+}
