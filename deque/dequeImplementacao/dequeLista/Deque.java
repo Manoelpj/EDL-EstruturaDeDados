f42b80cd72083fec16468fa3fda55821e9abd298
Manoelpj <manoelpinto2005@gmail.com>
2025-09-30 23:32:25 -0300

    feat: adicionar interface Deque

---
+++ deque/dequeImplementacao/dequeLista/Deque.java
@@ -0,0 +1,19 @@
+package dequeLista;
+
+public interface Deque{
+    public void addFirst(Object objeto);
+
+    public void addLast(Object objeto);
+
+    public Object removeFirst();
+
+    public Object removeLast();
+
+    public Object first();
+
+    public Object last();
+
+    public int size();
+
+    public boolean isEmpty();
+}
