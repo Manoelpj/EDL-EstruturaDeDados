946687bc9df85ed6322f6c1f35b408dda8d14eea
Manoel Jr <120431500+Manoelpj@users.noreply.github.com>
2025-10-07 18:27:28 +0000

    feat: modificar testes da pilha com array

--- pilha/pilhaImplementacao/PilhaMainArray.java
+++ pilha/pilhaImplementacao/PilhaMainArray.java
@@ -1,31 +1,23 @@
 public class PilhaMainArray{
     public static void main(String[] args){
-        //metodos testados
-            //push, fc do push, isEmpty, size, top, pop
-
-        PilhaArray pilha = new PilhaArray(2, 0);
+        PilhaArray pilha = new PilhaArray(1, 0);
         
-        pilha.push(111);
-        pilha.push(222);
-        pilha.push(333);
-        // System.out.println(pilha.isEmpty());
+        pilha.push(1);
+        pilha.push(2);
+        pilha.push(3);
+        pilha.push(4);
+        pilha.push(5);
+
+        pilha.sout(); 
+        System.out.println("Há " + pilha.size() + " elementos na pilha"); 
         
-        // System.out.println("Há " + pilha.size() + " elementos na pilha");
-
-        pilha.push(444);
-
-        // System.out.println("Há " + pilha.size() + " elementos na pilha"); 
-        // System.out.println(pilha.top()); 
-
         pilha.pop();
-        // System.out.println(pilha.top()); //removeu o último elemento
+        System.out.println(pilha.top());
         pilha.pop();
         pilha.pop();
         pilha.pop();
-        
-        //Ambos disparam erro
-        // System.out.println(pilha.pop());
-        // System.out.println(pilha.top());
-        // System.out.println(pilha.isEmpty());
+        pilha.pop();
+        System.out.println(pilha.isEmpty());
+        System.out.println(pilha.size());
     } 
 }
