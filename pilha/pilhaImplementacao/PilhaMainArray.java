f23112a9c76605159c64dc98dc3cb1563f4a7d1f
Manoel Jr <120431500+Manoelpj@users.noreply.github.com>
2025-10-08 20:35:18 +0000

    feat: adicionar teste para o método adicionarPilha() da questao 07 da lista 1 de pilha

--- pilha/pilhaImplementacao/PilhaMainArray.java
+++ pilha/pilhaImplementacao/PilhaMainArray.java
@@ -1,6 +1,7 @@
 public class PilhaMainArray{
     public static void main(String[] args){
         PilhaArray pilha = new PilhaArray(1, 0);
+        PilhaArray pilha2 = new PilhaArray(5,0);
         
         pilha.push(1);
         pilha.push(2);
@@ -8,16 +9,36 @@ public class PilhaMainArray{
         pilha.push(4);
         pilha.push(5);
 
+
+        pilha2.push("A");
+        pilha2.push("B");
+        pilha2.push("C");
+        pilha2.push("D");
+        pilha2.push("E");
+
+
+        pilha.adicionarPilha(pilha2);
         pilha.sout(); 
-        System.out.println("Há " + pilha.size() + " elementos na pilha"); 
+
+
+
         
-        pilha.pop();
-        System.out.println(pilha.top());
-        pilha.pop();
-        pilha.pop();
-        pilha.pop();
-        pilha.pop();
-        System.out.println(pilha.isEmpty());
-        System.out.println(pilha.size());
+        // System.out.println("Há " + pilha.size() + " elementos na pilha"); 
+        
+        // pilha.pop();
+        // pilha.pop();
+        // pilha.pop();
+        // System.out.println(pilha.top());
+        // System.out.println(pilha.isEmpty());
+        // System.out.println(pilha.size());
+
+
+
+
+        //teste do exercicio 04 - método empty()
+        // pilha.empty();
+        // System.out.println(pilha.size());
+        // System.out.println(pilha.isEmpty());
+        // pilha.sout(); 
     } 
 }
