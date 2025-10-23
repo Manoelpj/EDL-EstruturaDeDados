1dc419fa481bdea1f7e8865660c1b2a9e68f52be
Manoel Jr <120431500+Manoelpj@users.noreply.github.com>
2025-10-23 17:49:00 +0000

    bug: problemas no for do enqueue o size uma hora retorna 0 mesmo com valores dentro

--- fila/filaImplementacao/FilaArray.java
+++ fila/filaImplementacao/FilaArray.java
@@ -9,52 +9,54 @@ public class FilaArray implements Fila{
     public FilaArray(int capacidade){
         this.capacidade = capacidade;
         this.array = new Object[capacidade];
-        this.inicioFila = -1;
-        this.finalFila = 0; //final da fila deve estar sempre vazio e ser um indice na frente de inicio justamente pra manter o array circular
+        this.inicioFila = 0;
+        this.finalFila = 0;
         this.elementoFinal = "F";
 
     }
 
     @Override
     public void enqueue(Object objeto){
-        if(size() == capacidade){ //fila cheia
+        if(size() >= capacidade-1){ //fila cheia
             Object[] array2 = new Object[capacidade*2]; //duplica a capacidade e cria um array novo
-            iInicioFila = inicioFila
+            int iInicioFila = inicioFila;
             for(int i = 0; i < capacidade-1; i++){
                 array2[i] = array[iInicioFila++];
                 iInicioFila = (iInicioFila+1) % capacidade;
             }
             array = array2;
+            capacidade*=2;
         }
         array[finalFila] = objeto;
-        finalFila = (finalFila+1) % capacidade //atualiza o indice de finalFila de maneira circular
-        array[finalFila] = "F" //TESTES NAO MANTER final da fila recebe "F" TESTES NAO MANTER\\
+        finalFila = (finalFila+1) % capacidade; //atualiza o indice de finalFila de maneira circular
+        array[finalFila] = "F"; //TESTES NAO MANTER final da fila recebe "F" TESTES NAO MANTER\\
     }
 
     public Object dequeue() throws FilaVaziaException{
-        if(inicioFila < 0){
-            throw new FilaVaziaException("A fila está vazia")
+        if(inicioFila + finalFila == 0){
+            throw new FilaVaziaException("A fila está vazia");
         }
-        // vazia exception
-        Obejct valorTEMP = array[inicioFila];
-        array[inicioFila] = "APAGADO" //TESTES NAO MANTER por valor pra ter noção de que foi apagado TESTE NAO MANTER\\
+
+        Object valorTEMP = array[inicioFila];
+        array[inicioFila] = "APAGADO"; //TESTES NAO MANTER por valor pra ter noção de que foi apagado TESTE NAO MANTER\\
         inicioFila = (inicioFila+1) % capacidade;
+        array[inicioFila] = "I"; //TESTES NAO MANTER por valor pra ter noção de que foi apagado TESTE NAO MANTER\\
         return valorTEMP;
     }
 
     public Object first() throws FilaVaziaException{
-        if(inicioFila < 0){
-            throw new FilaVaziaException("A fila está vazia")
+        if(inicioFila + finalFila == 0){
+            throw new FilaVaziaException("A fila está vazia");
         }
-        return inicioFila;
+        return array[inicioFila];
     }
 
     public int size(){
-        return (finalFinal - inicioFila + capacidade) % capacidade;
+        return (capacidade - inicioFila + finalFila) % capacidade;
     }
 
     public boolean isEmpty(){
-        return inicioFila == -1;
+        return inicioFila == finalFila;
     }
 
 
@@ -64,33 +66,3 @@ public class FilaArray implements Fila{
     }
     // MÉTODO PARA TESTES: SOUT \\
 }
-
-
-
-
-
-
-
-        // if(size() >= capacidade){ //fila cheia
-        //     if(fatorCrescimento == 0){ 
-        //         capacidade*=2;
-        //     }else{
-        //         capacidade+=fatorCrescimento;
-        //     }
-
-        //     Object[] array2 = new Object[capacidade]; //capacidade aumentada já;
-        //     for(int i = 0; inicioFila < finalFila; i++){ //OBS: considerando que após o array aumentar sua capacidade os elementos se mantêm no mesmo indice, pois eles não precisam estar no final do array.
-        //         array2[i] = array[inicioFila];
-        //         inicioFila = (inicioFila +1) % capacidade;
-        //     }
-        //     inicioFila=0; //após o aumento do array, na realocação dos elementos o inicio da fila será sempre o indíce 0;
-        // }
-        // array[finalFila] = objeto;
-        // finalFila = (finalFila + 1) % capacidade; //o resto da divisão informa o indice que é para ir o finalFila após a adição de mais um elemento na fila, permite que o array seja circular.
-        
-        // //alternativa
-        // // if(finalFila == capacidade-1){ //finalFila está no último indice, quando inserir tem que ir para o indice 0
-        // //     array[finalFila] = objeto;
-        // //     finalFila = 0;
-        // // }
-        // // array[finalFila] = objeto;
--- fila/filaImplementacao/FilaMain.java
+++ fila/filaImplementacao/FilaMain.java
@@ -1,24 +1,33 @@
 public class FilaMain{
     public static void main(String[] args){
-        FilaArray fila = new FilaArray(10,0);
+        FilaArray fila = new FilaArray(4);
 
         fila.enqueue(1);
+        fila.dequeue();
         fila.enqueue(2);
         fila.enqueue(3);
+
         fila.enqueue(4);
-        fila.enqueue(5);
-        fila.enqueue(6);
-        fila.enqueue(7);
-        fila.enqueue(8);
-        fila.enqueue(9);
-        fila.enqueue(10);
-        fila.enqueue(11); //nao dobrou o array, mas está funcionando circular
-        System.out.println(fila.dequeue()); 
-        fila.enqueue("A"); //era pra ter sido adicionado no 11
+        System.out.println("tamanho da fila: "+fila.size());
 
         fila.sout(); // visualizar array no terminal
     }
 }
 
+//enqueue:
+//     acertos:
+//         o array está duplicando logo após adicionar o objeto no último lugar disponível dentro da fila, que é no espaço de finalFila
+//     erros:
+//         erro no for, alguns numeros não estão indo na cópia do array
 
-//OBS: fazer o finaFila ser um elemento de valor "F" no array, pra eu ter noção de onde ele está.
+//dequeue:
+//     até agr está funcionando normal
+
+//first
+//     tudo ok
+
+// size
+//     retorna normal até uma parte, dps ele fica retornando 0. ???????
+
+// isEmpty
+//     tudo ok
