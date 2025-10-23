94f97a3a8f8f6468fcaa5381649114b804d016ee
MANOEL PINTO JUNIOR <20241014040046@ifrn.local>
2025-10-23 14:05:32 -0300

    bug: resolver problemas de duplicação de array, alocação de objeto no array e retorno de first()

--- fila/filaImplementacao/FilaArray.java
+++ fila/filaImplementacao/FilaArray.java
@@ -1,91 +1,96 @@
 import java.util.Arrays;
 
 public class FilaArray implements Fila{
-    public int capacidade, fatorCrescimento, inicioFila, finalFila;
+    public int capacidade, inicioFila, finalFila;
     public Object[] array; //circular
+    public Object elementoFinal;
 
 
-    public FilaArray(int capacidade ,int fatorCrescimento){
+    public FilaArray(int capacidade){
         this.capacidade = capacidade;
-        this.fatorCrescimento = fatorCrescimento;
         this.array = new Object[capacidade];
-        this.inicioFila = 0;
-        this.finalFila = 0;
+        this.inicioFila = -1;
+        this.finalFila = 0; //final da fila deve estar sempre vazio e ser um indice na frente de inicio justamente pra manter o array circular
+        this.elementoFinal = "F";
+
     }
 
-    // ENQUEUE \\
     @Override
     public void enqueue(Object objeto){
-        if(size() >= capacidade){ //fila cheia
-            if(fatorCrescimento == 0){ 
-                capacidade*=2;
-            }else{
-                capacidade+=fatorCrescimento;
+        if(size() == capacidade){ //fila cheia
+            Object[] array2 = new Object[capacidade*2]; //duplica a capacidade e cria um array novo
+            iInicioFila = inicioFila
+            for(int i = 0; i < capacidade-1; i++){
+                array2[i] = array[iInicioFila++];
+                iInicioFila = (iInicioFila+1) % capacidade;
             }
-
-            Object[] array2 = new Object[capacidade]; //capacidade aumentada já;
-            for(int i = 0; inicioFila < finalFila; i++){ //OBS: considerando que após o array aumentar sua capacidade os elementos se mantêm no mesmo indice, pois eles não precisam estar no final do array.
-                array2[i] = array[inicioFila];
-                inicioFila = (inicioFila +1) % capacidade;
-            }
-            inicioFila=0; //após o aumento do array, na realocação dos elementos o inicio da fila será sempre o indíce 0;
+            array = array2;
         }
         array[finalFila] = objeto;
-        finalFila = (finalFila + 1) % capacidade; //o resto da divisão informa o indice que é para ir o finalFila após a adição de mais um elemento na fila, permite que o array seja circular.
-        
-        //alternativa
-        // if(finalFila == capacidade-1){ //finalFila está no último indice, quando inserir tem que ir para o indice 0
-        //     array[finalFila] = objeto;
-        //     finalFila = 0;
-        // }
-        // array[finalFila] = objeto;
+        finalFila = (finalFila+1) % capacidade //atualiza o indice de finalFila de maneira circular
+        array[finalFila] = "F" //TESTES NAO MANTER final da fila recebe "F" TESTES NAO MANTER\\
     }
-    // ENQUEUE \\
 
-
-    // DEQUEUE \\
-    @Override
-    public Object dequeue() {
-        if(finalFila == inicioFila){
-            throw new FilaVaziaException("A fila está vazia");
+    public Object dequeue() throws FilaVaziaException{
+        if(inicioFila < 0){
+            throw new FilaVaziaException("A fila está vazia")
         }
-        Object dequeueObjeto = array[inicioFila];
-        inicioFila = (inicioFila +1) % capacidade; //o resto da divisão informa o indice que é para ir o inicioFila após a removção de um elemento na fila, permite que o array seja circular.
-        return dequeueObjeto;
+        // vazia exception
+        Obejct valorTEMP = array[inicioFila];
+        array[inicioFila] = "APAGADO" //TESTES NAO MANTER por valor pra ter noção de que foi apagado TESTE NAO MANTER\\
+        inicioFila = (inicioFila+1) % capacidade;
+        return valorTEMP;
     }
-    // DEQUEUE \\
 
-
-    // FIST \\
-    @Override
-    public Object first() throws FilaVaziaException{ //proximo a sair da fila
-        if(finalFila == inicioFila){
-            throw new FilaVaziaException("A fila está vazia");
+    public Object first() throws FilaVaziaException{
+        if(inicioFila < 0){
+            throw new FilaVaziaException("A fila está vazia")
         }
         return inicioFila;
     }
-    // FIST \\
 
-
-    // SIZE \\
-    @Override
     public int size(){
-        return (capacidade - inicioFila + finalFila) % capacidade; //o resto da divisão informa a quantidade de elementos presentes no array.
+        return (finalFinal - inicioFila + capacidade) % capacidade;
     }
-    // SIZE \\
 
-
-    // ISEMPTY \\    
-    @Override
     public boolean isEmpty(){
-        return finalFila == inicioFila;
+        return inicioFila == -1;
     }
-    // ISEMPTY \\
 
-    
-    // SOUT \\
+
+    // MÉTODO PARA TESTES: SOUT \\
     public void sout(){
         System.out.println(Arrays.toString(array));
     }
-    // SOUT \\
+    // MÉTODO PARA TESTES: SOUT \\
 }
+
+
+
+
+
+
+
+        // if(size() >= capacidade){ //fila cheia
+        //     if(fatorCrescimento == 0){ 
+        //         capacidade*=2;
+        //     }else{
+        //         capacidade+=fatorCrescimento;
+        //     }
+
+        //     Object[] array2 = new Object[capacidade]; //capacidade aumentada já;
+        //     for(int i = 0; inicioFila < finalFila; i++){ //OBS: considerando que após o array aumentar sua capacidade os elementos se mantêm no mesmo indice, pois eles não precisam estar no final do array.
+        //         array2[i] = array[inicioFila];
+        //         inicioFila = (inicioFila +1) % capacidade;
+        //     }
+        //     inicioFila=0; //após o aumento do array, na realocação dos elementos o inicio da fila será sempre o indíce 0;
+        // }
+        // array[finalFila] = objeto;
+        // finalFila = (finalFila + 1) % capacidade; //o resto da divisão informa o indice que é para ir o finalFila após a adição de mais um elemento na fila, permite que o array seja circular.
+        
+        // //alternativa
+        // // if(finalFila == capacidade-1){ //finalFila está no último indice, quando inserir tem que ir para o indice 0
+        // //     array[finalFila] = objeto;
+        // //     finalFila = 0;
+        // // }
+        // // array[finalFila] = objeto;
