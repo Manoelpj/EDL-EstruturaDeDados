9b11e9c6fa1526e6fab55178769856cd667126a9
Manoelpj <manoelpinto2005@gmail.com>
2025-11-11 03:46:12 -0300

    feat: adicionar testes para os métodos do Vetor

---
+++ vetor/vetorImplementacao/vetorArray/VetorArrayMain.java
@@ -0,0 +1,18 @@
+package vetor.vetorImplementacao.vetorArray;
+
+public class VetorArrayMain {
+    public static void main(String[] args){
+        VetorArray vetor = new VetorArray(5);
+
+        vetor.insertAtRank(0, "1");
+        vetor.insertAtRank(1, "2");
+        vetor.insertAtRank(2, "3");
+        vetor.insertAtRank(1, "ALOU");
+        System.out.println(vetor.removeAtRank(0)); 
+        vetor.insertAtRank(3, "ALOU");
+
+        // System.out.println(vetor.size());
+        // System.out.println(vetor.isEmpty());
+        vetor.sout();
+    }
+}
