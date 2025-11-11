d93550fb1e0b107e384d8f4c4108a27b5b9c37d3
Manoelpj <manoelpinto2005@gmail.com>
2025-11-11 03:47:41 -0300

    feat: adicionar mensagem de erro para tentativa dee manipulações de valores em um vetor vazio

---
+++ vetor/vetorImplementacao/vetorArray/VetorVazioExpection.java
@@ -0,0 +1,9 @@
+package vetor.vetorImplementacao.vetorArray;
+
+public class VetorVazioExpection extends RuntimeException{
+    public VetorVazioExpection(String err){
+        super(err);
+    }
+    
+}
+
