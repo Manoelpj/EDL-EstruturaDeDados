0fffd13a8f8a3f9ad92499aac2b42d2130b04288
Manoel Jr <120431500+Manoelpj@users.noreply.github.com>
2025-11-16 20:30:51 +0000

    feat: adicionar os métodos da interface pilha na classe da lista duplamente ligada

---
+++ lista/listaImplementacao/ListaDuplamenteLigada.java
@@ -0,0 +1,67 @@
+public class ListaDuplamenteLigada implements Lista{
+    public Node no;
+    public Node head, tail;
+
+    public ListaDuplamenteLigada(){
+        this.head = this.tail = null;
+    }
+
+
+    public Object first(){
+
+    }
+
+    public Object last(){
+
+    }
+
+    public Object before(Object objeto){
+
+    }
+
+    public Object after(Object objeto){
+
+    }
+
+
+    public void replaceElements(Node no, Object objeto){
+
+    }
+
+    public void swapElements(Node no, Object objeto){
+
+    }
+
+    public void insertBefore(Node no, Object objeto){
+
+    }
+
+    public void insertAfter(Node no, Object objeto){
+
+    }
+
+    public Object remove(Node no){
+
+    }
+
+
+
+    public boolean isFirst(Object objeto){
+
+    }    
+
+    public boolean isLast(Object objeto){
+
+    }
+
+
+    public int size(){
+
+    }
+
+    public boolean isEmpty(){
+
+    }
+
+
+}
