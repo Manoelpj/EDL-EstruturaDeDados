4a2a6e6e48309509ba2e935990f29ebf0d2c24dc
Manoel Jr <120431500+Manoelpj@users.noreply.github.com>
2025-11-18 13:36:16 +0000

    feat: criar classe de testes para sequencia com lista duplamente ligada

---
+++ sequencia/sequenciaImplementacao/SequenciaListaMain.java
@@ -0,0 +1,15 @@
+package sequencia.sequenciaImplementacao;
+
+public class SequenciaListaMain{
+    public static void main(String[] args) {
+        SequenciaLista sequencia = new SequenciaLista();
+
+        sequencia.insertFirst(1);
+        sequencia.insertFirst(2);
+        sequencia.insertLast(3);
+        sequencia.replaceAtRank(0, 111);
+        System.out.println(sequencia.elemAtRank(2));
+        sequencia.insertAtRank(0, 000);
+        sequencia.lista.sout();
+    }
+}
